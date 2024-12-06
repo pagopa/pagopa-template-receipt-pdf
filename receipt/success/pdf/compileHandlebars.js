582f0729a994bcb4002e3b4f69c007e350bec28c
Damiano Plebani <damiano.plebani@pagopa.it>
2024-12-06 10:23:58 +0100

    Add helpers to avoid code repetition

--- receipt/success/pdf/compileHandlebars.js
+++ receipt/success/pdf/compileHandlebars.js
@@ -1,21 +1,32 @@
 const fs = require("fs");
+let { readFileSync, readdirSync } = require('fs');
 const Handlebars = require("handlebars");
+const path = require('node:path');
 
-// Import helpers
-const eq = require("../helpers/eq.js");
-const not = require("../helpers/not.js");
-const splitAndSpace = require("../helpers/splitAndSpace");
-const lowercase = require("../helpers/lowercase.js");
-const slice = require("../helpers/slice.js");
-const gt = require("../helpers/gt.js");
+// Utils functions
+const getFiles = source => readdirSync(source, { withFileTypes: true })
+  .filter(dirent => !dirent.isDirectory())
+  .map(dirent => dirent.name)
+const importFile = (filePath, fileName) => readFileSync(`${filePath}/${fileName}`, "utf8");
+const requireFile = (filePath, fileName) => require(`${filePath}/${fileName}`);
 
-// Register the helpers
-Handlebars.registerHelper("eq", eq);
-Handlebars.registerHelper("not", not);
-Handlebars.registerHelper("splitAndSpace", splitAndSpace);
-Handlebars.registerHelper("lowercase", lowercase);
-Handlebars.registerHelper("slice", slice);
-Handlebars.registerHelper("gt", gt);
+// Dynamically imports helpers & partials
+const helpersPath = '../helpers';
+const partialPath = `./partials`;
+
+const helperFiles = getFiles(`${helpersPath}`);
+for (let helperFile of helperFiles) {
+  const helper = requireFile(`${helpersPath}/`, path.parse(`${helpersPath}/${helperFile}`).name);
+  Handlebars.registerHelper(
+    path.parse(`${helpersPath}/${helperFile}`).name, helper);
+}
+
+const partialFiles = getFiles(`${partialPath}`);
+for (let partialFile of partialFiles) {
+  const partial = importFile(`${partialPath}/`, partialFile);
+  Handlebars.registerPartial(
+    path.parse(`${partialPath}/${partialFile}`).name, partial);
+}
 
 const templateFile = fs.readFileSync("template.hbs", "utf8");
 const template = Handlebars.compile(templateFile);
